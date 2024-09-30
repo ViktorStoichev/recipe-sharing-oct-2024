b07ba092d7c34a6c505863a051000d6a49b7eca4
ViktorStoichev <viktor.stoichev888@abv.bg>
2024-09-30 09:38:12 +0300

    Add resources, build architecture and render home page

---
+++ routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import { homeController } from "./controllers/home-controller.js";
+
+const router = Router();
+
+router.use(homeController);
+
+export const routes = router;
